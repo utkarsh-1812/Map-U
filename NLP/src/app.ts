66dd125a2d3cb2fa408206f3988d1ee9d714aa54
Ayush <ayushsingh.8d@gmail.com>
2023-04-08 22:40:37 +0530

    Add files via upload

---
+++ NLP/src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { getSentiment } from "./nlp";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.listen(4000, () => console.log("App is running http://localhost:4000"));
+
+app.get("/health", (req, res) => res.send(200));
+
+app.post("/api/sentiment", (req, res) => {
+  try {
+    const data = req.body.data;
+    console.log(data);
+  
+    const sentiment = getSentiment(data);
+  
+    return res.send({ sentiment });
+  }
+  catch(e){
+    console.log(e);
+  }
+ 
+});
